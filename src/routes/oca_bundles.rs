15fa2fa2a12e034ffeb75790faec0d4d6b0d631a
Marcin Olichwiruk <21108638+olichwiruk@users.noreply.github.com>
2023-08-18 10:56:15 +0200

    feat: add `expand` query parameter to /oca-bundles/{said}/steps endpint

--- src/routes/oca_bundles.rs
+++ src/routes/oca_bundles.rs
@@ -1,5 +1,6 @@
 use oca_rs::data_storage::DataStorage;
 use actix_web::{http::header::ContentType, web, HttpRequest, HttpResponse};
+use serde::{Serialize, Deserialize};
 
 pub async fn add_oca_file(
     db: web::Data<Box<dyn DataStorage>>,
@@ -53,10 +54,24 @@ pub async fn get_oca_bundle(
         .body(serde_json::to_string(&result).unwrap())
 }
 
+#[derive(Deserialize)]
+pub struct OCAFileHistoryQueryParams {
+    expand: Option<bool>,
+}
+
 pub async fn get_oca_file_history(
     db: web::Data<Box<dyn DataStorage>>,
     req: HttpRequest,
+    query_params: web::Query<OCAFileHistoryQueryParams>,
 ) -> HttpResponse {
+    #[derive(Serialize)]
+    struct Item {
+        from: Option<serde_value::Value>,
+        operation: serde_json::Value,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        oca_bundle: Option<serde_json::Value>,
+    }
+
     let said = req.match_info().get("said").unwrap().to_string();
 
     let oca_facade = oca_rs::Facade::new(db.get_ref().clone());
@@ -64,10 +79,17 @@ pub async fn get_oca_file_history(
         Ok(oca_build_steps) => {
             serde_json::to_value(
                 &oca_build_steps.iter().map(|s| {
-                    serde_json::json!({
-                        "from": s.parent_said,
-                        "operation": s.command,
-                    })
+                    serde_json::to_value(
+                        &Item {
+                            from: s.parent_said.clone().map(|said| serde_value::Value::String(said.to_string())),
+                            operation: serde_json::to_value(&s.command).unwrap(),
+                            oca_bundle: if query_params.expand.unwrap_or(false) {
+                                Some(serde_json::to_value(&s.result).unwrap())
+                            } else {
+                                None
+                            },
+                        }
+                    ).unwrap()
                 }).collect::<Vec<serde_json::Value>>()
             ).unwrap()
         },
