8b736e9bf4e172f201edc17b323ed04aff82ed3c
Marcin Olichwiruk <21108638+olichwiruk@users.noreply.github.com>
2023-09-12 22:40:31 +0200

    feat: add support for pagination in /internal/oca-bundles and /internal/capture-bases endpoints

--- src/routes/internal.rs
+++ src/routes/internal.rs
@@ -3,26 +3,34 @@ use oca_rs::{
     data_storage::DataStorage, repositories::SQLiteConfig, EncodeBundle,
 };
 
+#[derive(serde::Deserialize)]
+pub struct FetchAllOCABundleParams {
+    page: Option<usize>,
+}
+
 pub async fn fetch_all_oca_bundle(
     db: web::Data<Box<dyn DataStorage>>,
     cache_storage: web::Data<SQLiteConfig>,
+    query_params: web::Query<FetchAllOCABundleParams>,
 ) -> HttpResponse {
     let oca_facade = oca_rs::Facade::new(
         db.get_ref().clone(),
         cache_storage.get_ref().clone(),
     );
-    let result = match oca_facade.fetch_all_oca_bundle(500) {
-        Ok(oca_bundles) => {
+    let page = query_params.page.unwrap_or(1);
+    let result = match oca_facade.fetch_all_oca_bundle(100, page) {
+        Ok(all_oca_bundles) => {
             serde_json::json!({
                 "success": true,
-                "results":
-                    oca_bundles.iter().map(|oca_bundle| {
+                "r":
+                    all_oca_bundles.records.iter().map(|oca_bundle| {
                         serde_json::from_str(
                             &String::from_utf8(
                                 oca_bundle.encode().unwrap()
                             ).unwrap()
                         ).unwrap()
                     }).collect::<Vec<serde_json::Value>>(),
+                "m": all_oca_bundles.metadata,
             })
         }
         Err(errors) => {
@@ -38,19 +46,27 @@ pub async fn fetch_all_oca_bundle(
         .body(serde_json::to_string(&result).unwrap())
 }
 
+#[derive(serde::Deserialize)]
+pub struct FetchAllCaptureBaseParams {
+    page: Option<usize>,
+}
+
 pub async fn fetch_all_capture_base(
     db: web::Data<Box<dyn DataStorage>>,
     cache_storage: web::Data<SQLiteConfig>,
+    query_params: web::Query<FetchAllCaptureBaseParams>,
 ) -> HttpResponse {
     let oca_facade = oca_rs::Facade::new(
         db.get_ref().clone(),
         cache_storage.get_ref().clone(),
     );
-    let result = match oca_facade.fetch_all_capture_base(500) {
-        Ok(capture_bases) => {
+    let page = query_params.page.unwrap_or(1);
+    let result = match oca_facade.fetch_all_capture_base(100, page) {
+        Ok(all_capture_bases) => {
             serde_json::json!({
                 "success": true,
-                "results": capture_bases,
+                "r": all_capture_bases.records,
+                "m": all_capture_bases.metadata,
             })
         }
         Err(errors) => {
